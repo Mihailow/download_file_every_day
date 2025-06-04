2d2ef280401ae55fd0b170d233e0ec804865bceb
Михаил <70744799+Mihailow@users.noreply.github.com>
2025-06-04 16:14:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
-url_date_start = "03/10/2023"
-url_date_end = "18/10/2023"
+url_date_start = ""
+url_date_end = ""
 
 
 def find_files(url):
@@ -18,11 +18,12 @@ def download(url):
     with open('1.xml', 'wb') as f:
         f.write(r.content)
 
-
-url_begin = "https://office.gemotest.ru/OrderPrint_Xml.php?DateStart="
-url_middle = "&DateEnd="
-url_end = "&pp=23d927e006b320a11af39d6d701cf169"
-
-list_of_links = find_files(url_begin + url_date_start + url_middle + url_date_end + url_end)
-end = list_of_links[0].replace("×tamp", "&timestamp")
-download("https://office.gemotest.ru" + end)
+while(True):
+    url_begin = "https://office.gemotest.ru/"
+    url_middle = ""
+    url_end = ""
+    
+    list_of_links = find_files(url_begin + url_date_start + url_middle + url_date_end + url_end)
+    end = list_of_links[0].replace("×tamp", "&timestamp")
+    download("https://office.gemotest.ru" + end)
+    time.sleep(60*60*24)
